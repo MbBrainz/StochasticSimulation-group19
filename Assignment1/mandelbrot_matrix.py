29f2b484cfaf058476ced22fe9530b45d14a9cfa
Maurits Bos <Maurits.bos@ubiqu.eu>
2021-11-11 19:06:12 +0100

    moved mandelbrot calculation for random numbers to separate function

--- Assignment1/mandelbrot_matrix.py
+++ Assignment1/mandelbrot_matrix.py
@@ -35,3 +35,33 @@ def mandelbrot_matrix(N=200, re_lim=(-2,1),im_lim=(-1,1), height=400, width=600)
         z = np.where(np.abs(z)<=2, np.square(z) + c_matrix, z)
 
     return [n, re, im]
+
+def random_mandelbrot_points(n_size, re_lim, im_lim, threshold=100):
+    """Generates array of uniformly distributed complex numbers and calculates the mandelbrot set for these numbers
+
+        Args:
+            n_size (int): size of the array of complex number
+            re_lim (tuple): lower and upperlimit of the real axis
+            im_lim (tuple): lower and upperlimit(real numbers) of the imaginary axis.
+            threshold (int, optional): threshold used to calculate the mandelbrot set. Defaults to 100.
+
+        Returns:
+            list: [0]array of real numbers, [1]Array of imaginary numbers, [2]array of n values from the mandelbrot set.
+    """
+    re = np.random.uniform(re_lim[0], re_lim[1], size=n_size) # type: ignore
+    im = np.random.uniform(im_lim[0], im_lim[1], size=n_size) # type: ignore
+
+    random_points = re+im*1j
+    # create the counter matrix "n" and the z matrix
+    n = np.zeros(n_size)
+    z = np.zeros(n_size)
+
+    MAX_ITER = threshold
+    # iterate until the threshold is reached
+    for k in range(MAX_ITER):
+        # add one to n_ij only if abs(z_ij[k])<=2
+        n = np.where(np.abs(z) <= 2, n+1, n)
+        # calculate z_ij[k+1] only if abs(z_ij[k])<=2
+        z = np.where(np.abs(z) <= 2, np.square(z) + random_points, z)
+
+    return re, im, n
