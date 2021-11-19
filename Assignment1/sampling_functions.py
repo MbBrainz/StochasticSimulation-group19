795bf5ed52222974d6e310f2b0c62e24de07b092
Cheema <Abhishek1@wcw-stud-145-109-41-18.wireless.uva.nl>
2021-11-19 12:35:47 +0100

    Adding halton and sobol quasi random sequences

--- Assignment1/sampling_functions.py
+++ Assignment1/sampling_functions.py
@@ -123,36 +123,49 @@ def generate_Orthogonal(n,re_lim=[-2,1], im_lim=[-1.25,1.25]):
 generate_Orthogonal(3)
 
 # %%
-def generate_Orthogonal_inPlace(n,re_lim=[-2,1], im_lim=[-1.25,1.25]):
-
-    width_x = re_lim[1]-re_lim[0]
-    width_y = im_lim[1]-im_lim[0]
-
-    num_sq = n*n
-    xlist = np.arange(num_sq).reshape((n,n))
-    ylist = np.arange(num_sq).reshape((n,n))
-    x_scale = width_x/(num_sq)
-    y_scale = width_y/(num_sq)
-    re = []
-    im = []
-
-    # inplace permutation
-    for i in range(n):
-        xlist[i] = np.random.permutation(xlist[i])
-        ylist[i] = np.random.permutation(ylist[i])
-    for i in range(n):
-        for j in range(n):
-            x = re_lim[0] + x_scale*(xlist[i][j] + np.random.uniform())
-            y = im_lim[0] + y_scale*(ylist[j][i] + np.random.uniform())
-            re.append(x)
-            im.append(y)
-
-    plot_latin(re,im)
+def generate_sobol(n, re_lim=[-2,1], im_lim=[-1.25,1.25]):
+   
+    re_im_sampler = qmc.Sobol(d = 2)
+    l_bounds = [re_lim[0], im_lim[0]]
+    u_bounds = [re_lim[1], im_lim[1]]
+    
+    num_samples = int(np.round(np.log2(n)))
+    print(num_samples)
+    
+    re_im_samples = re_im_sampler.random_base2(num_samples)
+    # 2**num_samples are generated ~ n
+    re_im_samples = qmc.scale(re_im_samples, l_bounds, u_bounds)
+    plt.scatter(re_im_samples[:,0],re_im_samples[:,1])
     plt.show()
-    #print(re)
-    #print(im)
-    re_all = np.array(re)
-    im_all = np.array(im)
-    return re_all+ im_all*1j
+    plot_latin(re_im_samples[:,0],re_im_samples[:,1])
+    plt.show()
+    re_im_samples = re_im_samples[:,0] + re_im_samples[:,1]*1j
+    re_im_samples = re_im_samples[:n]
+    
+    return re_im_samples
 
-#generate_Orthogonal(3)
+
+re_im_samples = generate_sobol(9)
+print(re_im_samples)
+
+# %%
+def generate_halton(n, re_lim=[-2,1], im_lim=[-1.25,1.25]):
+    
+    l_bounds = [re_lim[0], im_lim[0]]
+    u_bounds = [re_lim[1], im_lim[1]]
+    
+    re_im_sampler = qmc.Halton(d = 2)
+    
+    re_im_samples = re_im_sampler.random(n)
+    re_im_samples = qmc.scale(re_im_samples, l_bounds, u_bounds)
+    
+    plot_latin(re_im_samples[:,0],re_im_samples[:,1])
+    plt.show()
+    
+    re_im_samples = re_im_samples[:,0] + re_im_samples[:,1]*1j
+    return re_im_samples
+
+re_im_samples = generate_halton(9)
+print(re_im_samples)
+
+
